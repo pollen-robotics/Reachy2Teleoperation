4821c154bb9e4df1d374afe7e675b38f3f08f2c0
glannuzel <gaelle.lannuzel@gmail.com>
2023-12-15 12:47:00 +0100

    Add Lidar detection message

---
+++ Assets/Scripts/UI/WarningLidarObstacleDetectedUIManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Mobile.Base.Lidar;
+
+
+namespace TeleopReachy
+{
+    public class WarningLidarObstacleDetectedUIManager : MonoBehaviour
+    {
+        private DataMessageManager dataController;
+        private RobotStatus robotStatus;
+        private RobotConfig robotConfig;
+
+        [SerializeField]
+        private Text warningMessage;
+
+        private string messageToDisplay;
+
+        private bool needUpdateWarningMessage;
+        private bool wantWarningMessageDisplayed;
+
+        private Coroutine limitDisplayInTime;
+
+        private UserMobilityInput userMobilityInput;
+
+        void Start()
+        {
+            robotStatus = RobotDataManager.Instance.RobotStatus;
+            robotConfig = RobotDataManager.Instance.RobotConfig;
+
+            dataController.event_OnLidarDetectionUpdate.AddListener(ShowWarningMessage);
+            robotStatus.event_OnStopTeleoperation.AddListener(HideWarningMessage);
+
+            needUpdateWarningMessage = false;
+            wantWarningMessageDisplayed = false;
+
+            transform.ActivateChildren(false);
+        }
+
+        void ShowWarningMessage(LidarObstacleDetectionEnum lidarStatus)
+        {
+            if(lidarStatus == LidarObstacleDetectionEnum.ObjectDetectedSlowdown)
+            {
+                messageToDisplay = "Close to obstacles: mobile base slowed down";
+            }
+            else
+            {
+                messageToDisplay = "Obstacle detected: can't move in direction";
+            }
+            if (robotStatus.IsRobotTeleoperationActive())
+            {
+                wantWarningMessageDisplayed = true;
+                needUpdateWarningMessage = true;
+            }
+        }
+
+        void Update()
+        {
+            if(needUpdateWarningMessage)
+            {
+                warningMessage.text = messageToDisplay;
+                if(wantWarningMessageDisplayed) 
+                {
+                    if(limitDisplayInTime != null) StopCoroutine(limitDisplayInTime);
+                    limitDisplayInTime = StartCoroutine(DisplayLimitedInTime());
+                }
+                // else
+                // {
+                //     transform.ActivateChildren(false);
+                // }
+                needUpdateWarningMessage = false;
+            }
+        }
+
+        IEnumerator DisplayLimitedInTime()
+        {
+            transform.ActivateChildren(true);
+            yield return new WaitForSeconds(1);
+            transform.ActivateChildren(false);
+            wantWarningMessageDisplayed = false;
+            needUpdateWarningMessage = true;
+        }
+
+        void HideWarningMessage()
+        {
+            wantWarningMessageDisplayed = false;
+        }
+    }
+}
