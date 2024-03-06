3d20b6332162bce8a32e7a4a1809beef21d65d0d
glannuzel <gaelle.lannuzel@gmail.com>
2024-03-06 14:31:35 +0100

    Add right controller emergency stop

--- Assets/Scripts/UserInputs/UserEmergencyStopInput.cs
+++ Assets/Scripts/UserInputs/UserEmergencyStopInput.cs
@@ -16,6 +16,8 @@ namespace TeleopReachy
         private RobotStatus robotStatus;
 
         bool rightGripPressed;
+        bool rightTriggerPressed;
+        bool rightPrimaryButtonPressed;
         bool leftGripPressed;
         bool leftTriggerPressed;
         bool leftPrimaryButtonPressed;
@@ -52,15 +54,17 @@ namespace TeleopReachy
 
         void Update()
         {
-            // For joystick commands
             controllers.rightHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out rightGripPressed);
+            controllers.rightHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out rightTriggerPressed);
+            controllers.rightHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out rightPrimaryButtonPressed);
+            
             controllers.leftHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out leftGripPressed);
             controllers.leftHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out leftTriggerPressed);
             controllers.leftHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out leftPrimaryButtonPressed);
 
             if (robotStatus != null && robotStatus.IsRobotTeleoperationActive() && !robotStatus.AreRobotMovementsSuspended())
             {
-                if (leftGripPressed && leftTriggerPressed && leftPrimaryButtonPressed)
+                if ((leftGripPressed && leftTriggerPressed && leftPrimaryButtonPressed) || (rightGripPressed && rightTriggerPressed && rightPrimaryButtonPressed))
                 {
                     robotStatus.SuspendRobotTeleoperation();
                     event_OnEmergencyStopCalled.Invoke();
