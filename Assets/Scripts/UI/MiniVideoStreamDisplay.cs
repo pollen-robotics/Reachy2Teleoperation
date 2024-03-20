97632d40afc247a559145e66851a410a1628acca
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-03-20 14:01:04 +0100

    Bug #68: fix mini video screen, remove useless robotvideostream

--- Assets/Scripts/UI/MiniVideoStreamDisplay.cs
+++ Assets/Scripts/UI/MiniVideoStreamDisplay.cs
@@ -1,47 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+using GstreamerWebRTC;
 
-
-namespace TeleopReachy 
+namespace TeleopReachy
 {
     public class MiniVideoStreamDisplay : MonoBehaviour
     {
-        private RobotVideoStream videoStream;
-        private bool need_update_mini_viewer = false;
-        
+        private GstreamerUnityGStreamerPlugin audioVideoController;
+        private Renderer screen;
+
         // Used to update display when going back in mirror scene
         void Start()
         {
-            videoStream = RobotDataManager.Instance.RobotVideoStream;
-            videoStream.event_OnVideoTextureReady.AddListener(ReadyToUpdate);
-            UpdateTexture();
+            screen = GetComponent<Renderer>();
+            audioVideoController = WebRTCManager.Instance.webRTCVideoController;
+            screen.material.SetTexture("_LeftTex", audioVideoController.GetLeftTexture());
+            screen.material.SetTexture("_RightTex", audioVideoController.GetRightTexture());
         }
 
-        void ReadyToUpdate()
-        {
-            need_update_mini_viewer = true;
-        }
-
-        void UpdateTexture()
-        {
-            Texture tex = videoStream.GetLeftEyeTexture();
-            if(tex != null)
-            {
-                GetComponent<Renderer>().material.SetTexture("_LeftTex", tex);
-                GetComponent<Renderer>().material.SetTexture("_RightTex", tex);
-            }
-        }
-
-        void Update()
-        {
-            // Done only on connection
-            if (need_update_mini_viewer)
-            {
-                need_update_mini_viewer = false;
-                UpdateTexture();
-            }
-        }
     }
 }
