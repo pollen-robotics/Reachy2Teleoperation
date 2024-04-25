4bba17c8d7eb762eb579e40e6cc0ef6be3598467
ClaireHzl <126695207+ClaireHzl@users.noreply.github.com>
2024-04-25 17:17:33 +0200

    ajout CapturePose pendant la téléop + méthode INCIA à modifier

--- Assets/Scripts/UI/InstructionsTextUIManager.cs
+++ Assets/Scripts/UI/InstructionsTextUIManager.cs
@@ -12,6 +12,7 @@ namespace TeleopReachy
 
         //private ConnectionStatus connectionStatus;
         private TransitionRoomManager transitionRoomManager;
+        private WristCalibINCIA wristCalibINCIA;
 
         private TextButtonControllerModifier textButtonControllerModifier;
 
@@ -20,11 +21,33 @@ namespace TeleopReachy
         private string instructionsText;
         private string instructionsDetailsText;
 
+        private static InstructionsTextUIManager instance;
+
+        public static InstructionsTextUIManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<InstructionsTextUIManager>();
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(InstructionsTextUIManager).Name;
+                        instance = obj.AddComponent<InstructionsTextUIManager>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+
         void Start()
         {
             instructions = transform.GetComponent<Text>();
 
             transitionRoomManager = TransitionRoomManager.Instance;
+            wristCalibINCIA = WristCalibINCIA.Instance;
             textButtonControllerModifier = GetComponent<TextButtonControllerModifier>();
 
             if (Robot.IsCurrentRobotVirtual())
@@ -35,10 +58,14 @@ namespace TeleopReachy
             {
                 instructionsText = "Please face the mirror then press Ready";
                 instructionsDetailsText = "";
-                transitionRoomManager.event_OnReadyForTeleop.AddListener(IndicateToPressA);
-                transitionRoomManager.event_OnAbortTeleop.AddListener(IndicateRobotNotReady);
+    
             }
-
+            transitionRoomManager.event_OnReadyForTeleop.AddListener(IndicateToPressA);
+            transitionRoomManager.event_OnAbortTeleop.AddListener(IndicateRobotNotReady);
+            wristCalibINCIA.event_WaitForWristCalib.AddListener(IndicateToPressX);
+            wristCalibINCIA.event_StartRightWristCalib.AddListener(() => IndicateInitialCalibration("right"));
+            wristCalibINCIA.event_StartLeftWristCalib.AddListener(() => IndicateInitialCalibration("left"));
+            wristCalibINCIA.event_OnWristCalibChanged.AddListener(IndicateRobotReady);
             needUpdateText = true;
         }
 
@@ -64,6 +91,28 @@ namespace TeleopReachy
         {
             instructionsText = "Reachy is not ready for teleop.";
             instructionsDetailsText = "Some required services are missing. Check the status panel for more details!";
+            needUpdateText = true;
+        }
+
+        public void IndicateToPressX()
+        {
+            instructionsText = "Place arms on either side of body then Press " + textButtonControllerModifier.GetPrimLeftButtonName() + " to start the calibration";
+            instructionsText = textButtonControllerModifier.ChangeTextAccordingToController(instructionsText);
+            instructionsDetailsText = "";
+            needUpdateText = true;
+        }
+
+        public void IndicateInitialCalibration(string side)
+        {
+            instructionsText = "Calibration of your " + side + " wrist : " ;
+            instructionsDetailsText = "Keep your " + side + " arm straight and move your wrist in all directions.";
+            needUpdateText = true;
+        }
+
+        public void IndicateRobotReady()
+        {
+            instructionsText = "Reachy is ready for teleop, you can press 'Ready' to start";
+            instructionsDetailsText = "";
             needUpdateText = true;
         }
     }
