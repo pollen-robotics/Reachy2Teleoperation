45a33351618c640613381b4f13c00f1f46c0f707
ClaireHzl <126695207+ClaireHzl@users.noreply.github.com>
2024-05-02 17:30:06 +0200

    rescale avec valeur min et max des angles

--- Assets/Scripts/Miscellaneous/CaptureWristPose.cs
+++ Assets/Scripts/Miscellaneous/CaptureWristPose.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System.IO;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace TeleopReachy
 {
+    [System.Serializable]
+    public struct LinearParameters
+    {
+        public float A;
+        public float b;
+
+        public LinearParameters(float A, float b)
+        {
+            this.A = A;
+            this.b = b;
+        }
+    }
     public class CaptureWristPose : Singleton<CaptureWristPose>
     {
         public float recordInterval = 1f;
@@ -13,8 +26,16 @@ namespace TeleopReachy
         private Transform leftController;
         private Transform rightController;
         private Transform userTrackerTransform;
+        private List<Vector3> leftHandEulerAnglesList = new List<Vector3>();
+        private List<Vector3> rightHandEulerAnglesList = new List<Vector3>();
+        public Vector3 rightMinAngles;
+        public Vector3 rightMaxAngles;
+        public Vector3 leftMinAngles;
+        public Vector3 leftMaxAngles;
         public Quaternion rightNeutralOrientation;
         public Quaternion leftNeutralOrientation;
+        public List<LinearParameters> rightLinearParameters = new List<LinearParameters>();
+        public List<LinearParameters> leftLinearParameters = new List<LinearParameters>();
         private List<string> capturedData = new List<string>();
         private bool buttonX;
         private int nbPosition = 0;
@@ -22,6 +43,7 @@ namespace TeleopReachy
         private ControllersManager controllers;
         private RobotStatus robotStatus ;
         public UnityEvent event_NeutralPoseCaptured = new UnityEvent();
+        public UnityEvent event_WristPoseCaptured = new UnityEvent();
 
 
         public void Start()
@@ -52,9 +74,17 @@ namespace TeleopReachy
             }
 
             if (nbPosition ==7)
+            {
+                (leftMinAngles, leftMaxAngles) = GetMinAndMax(leftHandEulerAnglesList);
+                (rightMinAngles, rightMaxAngles) = GetMinAndMax(rightHandEulerAnglesList);
+                Debug.Log("Left Min Angles: " + leftMinAngles + " Left Max Angles: " + leftMaxAngles);
+                Debug.Log("Right Min Angles: " + rightMinAngles + " Right Max Angles: " + rightMaxAngles);
                 SavePoseData();
+                GetRescalingParameters();
+                event_WristPoseCaptured.Invoke();
                 nbPosition = 0;
                 capturedData.Clear();
+            }
         }
 
         public void StartWristCalibration()
@@ -78,8 +108,14 @@ namespace TeleopReachy
             rightHandEulerAngles.y = NormalizeAngle(rightHandEulerAngles.y);
             rightHandEulerAngles.z = NormalizeAngle(rightHandEulerAngles.z);
 
+
+
             nbPosition++;
+            Debug.Log("Nb position: " + nbPosition);
             buttonX = false;
+
+            leftHandEulerAnglesList.Add(leftHandEulerAngles);
+            rightHandEulerAnglesList.Add(rightHandEulerAngles);
 
             if (nbPosition == 1)
             {
@@ -120,10 +156,49 @@ namespace TeleopReachy
             return angle;
         }
 
+        public (Vector3, Vector3) GetMinAndMax (List<Vector3> list)
+        {
+
+            float maxX = list.Max(angle => angle.x);
+            float minX = list.Min(angle => angle.x);
+            float maxY = list.Max(angle => angle.y);
+            float minY = list.Min(angle => angle.y);
+            float maxZ = list.Max(angle => angle.z);
+            float minZ = list.Min(angle => angle.z);
+
+            Vector3 min = new Vector3(minX, minY, minZ);
+            Vector3 max = new Vector3(maxX, maxY, maxZ);
+
+            return (min, max);
+        }
+
+        public void GetRescalingParameters()
+        {
+            rightLinearParameters.Add(LinearCoefficient(rightMinAngles.x, rightNeutralOrientation.eulerAngles.x, -100f, 0f));
+            rightLinearParameters.Add(LinearCoefficient(rightMinAngles.y, rightNeutralOrientation.eulerAngles.y, -40f, 0f));
+            rightLinearParameters.Add(LinearCoefficient(rightMinAngles.z, rightNeutralOrientation.eulerAngles.z, -50f, 0f));
+            rightLinearParameters.Add(LinearCoefficient(rightNeutralOrientation.eulerAngles.x, rightMaxAngles.x, 0f, 100f));
+            rightLinearParameters.Add(LinearCoefficient(rightNeutralOrientation.eulerAngles.y, rightMaxAngles.y, 0f, 20f));
+            rightLinearParameters.Add(LinearCoefficient(rightNeutralOrientation.eulerAngles.z, rightMaxAngles.z, 0f, 50f));
+
+            leftLinearParameters.Add(LinearCoefficient(leftMinAngles.x, leftNeutralOrientation.eulerAngles.x, -100f, 0f));
+            leftLinearParameters.Add(LinearCoefficient(leftMinAngles.y, leftNeutralOrientation.eulerAngles.y, -40f, 0f));
+            leftLinearParameters.Add(LinearCoefficient(leftMinAngles.z, leftNeutralOrientation.eulerAngles.z, -50f, 0f));
+            leftLinearParameters.Add(LinearCoefficient(leftNeutralOrientation.eulerAngles.x, leftMaxAngles.x, 0f, 100f));
+            leftLinearParameters.Add(LinearCoefficient(leftNeutralOrientation.eulerAngles.y, leftMaxAngles.y, 0f, 20f));
+            leftLinearParameters.Add(LinearCoefficient(leftNeutralOrientation.eulerAngles.z, leftMaxAngles.z, 0f, 50f));
             
+        }
+
+        public LinearParameters LinearCoefficient(float x1, float x2, float y1, float y2)
+        {
+            float A = (y2 - y1) / (x2 - x1);
+            float B = y1 - A * x1;
+            LinearParameters linearParameters = new LinearParameters(A, B);
+            return linearParameters;
+        }
+
+
     }
-
-
-
 }
 
