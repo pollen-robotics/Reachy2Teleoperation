292ffe1b0f44267fff2aeee5b9da6359ee8a3f4b
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-05-03 14:10:09 +0200

    enhancement #136: fix dll, add additional lib folder to PATH in launch.bat

--- Installer/launch.bat
+++ Installer/launch.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;C:\gstreamer\1.0\msvc_x86_64\bin
+set PATH=%PATH%;C:\gstreamer\1.0\msvc_x86_64\bin;C:\gstreamer\1.0\msvc_x86_64\lib\gstreamer-1.0
 set GST_DEBUG_FILE=%userprofile%\Reachy2Teleoperation.log
 set GST_DEBUG=3
 Reachy2Teleoperation.exe
