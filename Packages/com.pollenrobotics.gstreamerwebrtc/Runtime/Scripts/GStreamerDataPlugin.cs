8af912db0e213d60d880b20bf0ef71e15ef4b3f5
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-06-07 15:41:18 +0200

    enhancement #142: gstreamer plugin handle data. first version

---
+++ Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/GStreamerDataPlugin.cs
@@ -0,0 +1,175 @@
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using AOT;
+using Codice.Client.BaseCommands;
+using UnityEngine.TextCore.Text;
+
+namespace GstreamerWebRTC
+{
+    public class GStreamerDataPlugin
+    {
+
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        private static extern void CreateDataPipeline();
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        private static extern void DestroyDataPipeline();
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        private static extern void SetSDPOffer(string sdp_offer);
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        private static extern void SetICECandidate(string candidate, int mline_index);
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        public static extern void SendBytesChannelService(byte[] array, int size);
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        static extern void RegisterICECallback(iceCallback cb);
+        delegate void iceCallback(IntPtr candidate, int size_candidate, int mline_index);
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        static extern void RegisterSDPCallback(sdpCallback cb);
+        delegate void sdpCallback(IntPtr request, int size);
+
+#if (PLATFORM_IOS || PLATFORM_TVOS || PLATFORM_BRATWURST || PLATFORM_SWITCH) && !UNITY_EDITOR
+    [DllImport("__Internal")]
+#else
+        [DllImport("UnityGStreamerPlugin")]
+#endif
+        static extern void RegisterChannelServiceOpenCallback(channelServiceOpenCallback cb);
+        delegate void channelServiceOpenCallback();
+
+        private string _signallingServerURL;
+        private Signalling _signalling;
+
+        public bool producer = false;
+        public string remote_producer_name = "grpc_webrtc_bridge";
+
+        public UnityEvent event_OnPipelineStarted;
+
+        private static UnityEvent<string> event_OnSDPAnswer;
+
+        private static UnityEvent<string, int> event_OnICE;
+        public static UnityEvent event_OnChannelServiceOpen;
+
+        public GStreamerDataPlugin(string ip_address)
+        {
+            RegisterICECallback(OnICECallback);
+            RegisterSDPCallback(OnSDPCallback);
+            RegisterChannelServiceOpenCallback(OnChannelServiceOpenCallback);
+
+            _signallingServerURL = "ws://" + ip_address + ":8443";
+
+            _signalling = new Signalling(_signallingServerURL, producer, remote_producer_name);
+
+            _signalling.event_OnRemotePeerId.AddListener(StartPipeline);
+            _signalling.event_OnSDPOffer.AddListener(OnSDPOffer);
+            _signalling.event_OnICECandidate.AddListener(OnReceivedICE);
+
+            event_OnPipelineStarted = new UnityEvent();
+
+            event_OnSDPAnswer = new UnityEvent<string>();
+            event_OnSDPAnswer.AddListener(SendSDPAnswer);
+
+            event_OnICE = new UnityEvent<string, int>();
+            event_OnICE.AddListener(OnICE);
+
+            event_OnChannelServiceOpen = new UnityEvent();
+        }
+
+        public void Connect()
+        {
+            _signalling.Connect();
+        }
+
+        void StartPipeline(string remote_peer_id)
+        {
+            Debug.Log("start pipe " + remote_peer_id);
+            CreateDataPipeline();
+            event_OnPipelineStarted.Invoke();
+        }
+
+        void OnSDPOffer(string sdp_offer)
+        {
+            SetSDPOffer(sdp_offer);
+        }
+
+        void OnICE(string candidate, int mline_index)
+        {
+            _signalling.SendICECandidate(candidate, mline_index);
+        }
+
+        void OnReceivedICE(string candidate, int mline_index)
+        {
+            SetICECandidate(candidate, mline_index);
+        }
+
+        void SendSDPAnswer(string sdp_answer)
+        {
+            _signalling.SendSDP(sdp_answer);
+        }
+
+        public void Cleanup()
+        {
+            _signalling.Close();
+            DestroyDataPipeline();
+        }
+
+        [MonoPInvokeCallback(typeof(iceCallback))]
+        static void OnICECallback(IntPtr candidate, int size_candidate, int mline_index)
+        {
+            string candidate_msg = Marshal.PtrToStringAnsi(candidate, size_candidate);
+            Debug.Log("ICE Candidate: " + candidate_msg + " " + mline_index);
+            event_OnICE.Invoke(candidate_msg, mline_index);
+        }
+
+        [MonoPInvokeCallback(typeof(sdpCallback))]
+        static void OnSDPCallback(IntPtr request, int size)
+        {
+            string sdp_answer = Marshal.PtrToStringAnsi(request, size);
+            Debug.Log("SDP " + sdp_answer);
+            event_OnSDPAnswer.Invoke(sdp_answer);
+        }
+
+        [MonoPInvokeCallback(typeof(channelServiceOpenCallback))]
+        static void OnChannelServiceOpenCallback()
+        {
+            event_OnChannelServiceOpen.Invoke();
+        }
+
+    }
+}
