359a53dd56979c2c55ea4e3a9fa7b3c737ba08f3
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-10-29 17:23:10 +0100

    enhancement #202: add copypirght and use refactorised plugin

--- Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/GstreamerPlugin.cs
+++ Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/GstreamerPlugin.cs
@@ -1,3 +1,7 @@
+/* Copyright(c) Pollen Robotics, all rights reserved.
+ This source code is licensed under the license found in the
+ LICENSE file in the root directory of this source tree. */
+
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading;
--- Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/SignallingMessages.cs
+++ Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/SignallingMessages.cs
@@ -1,5 +1,8 @@
-using System;
+/* Copyright(c) Pollen Robotics, all rights reserved.
+ This source code is licensed under the license found in the
+ LICENSE file in the root directory of this source tree. */
 
+using System;
 
 namespace GstreamerWebRTC
 {
