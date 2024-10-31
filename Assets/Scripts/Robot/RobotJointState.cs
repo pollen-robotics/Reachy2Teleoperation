abf076a98ffd4c9dcf5b738702a04d8019fdfedb
glannuzel <gaelle.lannuzel@gmail.com>
2024-10-31 17:49:01 +0100

    Remove commented lines

--- Assets/Scripts/Robot/RobotJointState.cs
+++ Assets/Scripts/Robot/RobotJointState.cs
@@ -14,27 +14,10 @@ namespace TeleopReachy
         {
             dataController = DataMessageManager.Instance;
             dataController.event_OnStateUpdatePresentPositions.AddListener(UpdateJointsState);
-
-            // EventManager.StartListening(EventNames.QuitMirrorScene, UpdateRobot);
-            // EventManager.StartListening(EventNames.MirrorSceneLoaded, UpdateModelRobot);
         }
-
-        // void UpdateRobot()
-        // {
-        //     reachy = GameObject.Find("Reachy2").GetComponent<Reachy2Controller.Reachy2Controller>();
-        // }
-
-        // void UpdateModelRobot()
-        // {
-        //     reachy = GameObject.Find("Reachy2Ghost").GetComponent<Reachy2Controller.Reachy2Controller>();
-        // }
 
         protected void UpdateJointsState(Dictionary<string, float> PresentPositions)
         {
-            // if (reachy != null)
-            // {
-            //     reachy.HandleCommand(PresentPositions);
-            // }
             event_OnPresentPositionsChanged.Invoke(PresentPositions);
         }
     }
