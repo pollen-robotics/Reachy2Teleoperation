8f0f31bf53581eaf7bc7689208b698520a8b1dad
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2025-08-31 19:54:46 +0200

    enhancement #298: update android plugin

--- Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/GstreamerPlugin.cs
+++ Packages/com.pollenrobotics.gstreamerwebrtc/Runtime/Scripts/GstreamerPlugin.cs
@@ -94,7 +94,7 @@ namespace GstreamerWebRTC
         }
 
 #if UNITY_ANDROID
-        private IEnumerator WaitForNativePointer(GStreamerRenderingPlugin renderingPlugin)
+        protected virtual IEnumerator WaitForNativePointer(GStreamerRenderingPlugin renderingPlugin)
         {
             leftRawImage.texture = renderingPlugin.SetTextures(true);
             rightRawImage.texture = renderingPlugin.SetTextures(false);
